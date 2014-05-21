c310d51a1093bacd174a57a0f415c57ed6dc4e7f
Rich Wareham <rjw57@cam.ac.uk>
2014-05-21 14:25:33 +0100

    configuration stub

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,18 @@
+var initialized = false;
+
+Pebble.addEventListener("ready", function() {
+  console.log("ready called!");
+  initialized = true;
+});
+
+Pebble.addEventListener("showConfiguration", function() {
+  console.log("showing configuration");
+  Pebble.openURL('https://rjw57.github.io/pebble-clubcard/configuration.html');
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  console.log("configuration closed");
+  // webview closed
+  var options = JSON.parse(decodeURIComponent(e.response));
+  console.log("Options = " + JSON.stringify(options));
+});
